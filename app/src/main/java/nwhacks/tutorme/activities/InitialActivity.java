62b325fe42ad62aad3f5a9deac46f95a6cb30c0f
Stephen van Son <stephenv95@gmail.com>
2016-02-27 22:06:37 -0700

    Added GPSTracker utility class to get the current location of the current user, later to be used in firebase query for finding relevant tutors.

--- app/src/main/java/nwhacks/tutorme/activities/InitialActivity.java
+++ app/src/main/java/nwhacks/tutorme/activities/InitialActivity.java
@@ -1,5 +1,7 @@
 package nwhacks.tutorme.activities;
 
+import android.content.Intent;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +9,7 @@ import com.firebase.client.Firebase;
 
 import nwhacks.tutorme.R;
 import nwhacks.tutorme.model.Tutor;
+import nwhacks.tutorme.utils.GPSTracker;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -21,6 +24,21 @@ public class InitialActivity extends AppCompatActivity {
         //hardcoded tutor
         Tutor tutor = new Tutor("John Smith", "johnsmith@johnsmith.com", new String[] {"math", "science"}, "$35/hr");
 
+        GPSTracker gpsTracker = new GPSTracker(getApplicationContext());
+
+        //get the users location
+        Location loc = gpsTracker.getLocation(getApplicationContext());
+
+
+
+    }
+
+
+
+
+    //to be called after signup/login
+    public void switchToMaps(){
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
 
     }
 
